8b562fe38c6029b171b6a15880b748aad7f3e128
wzhang4@umbc.edu <wzhang4@umbc.edu>
2026-05-08 00:41:55 -0400

    added env files

--- app/db.js
+++ app/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "projectdb",
-  user: process.env.DB_USER || "student1",
-  password: process.env.DB_PASSWORD || "student123"
+  host: process.env.PGHOST || "db",
+  port: process.env.PGPORT || 5432,
+  database: process.env.PGDATABASE || "projectdb",
+  user: process.env.PGUSER || "student1",
+  password: process.env.PGPASSWORD || "student123"
 });
 
 module.exports = pool;
--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
